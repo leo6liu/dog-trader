63af334e0f2bd833b3f45e51e8b80df5c2da5297
Ethan <ethanchang34@yahoo.com>
2023-04-10 02:03:08 -0400

    set up gpt go module

--- playground/hello/go.mod
+++ playground/hello/go.mod
@@ -1,3 +1,5 @@
 module udon.studio/home
 
 go 1.20
+
+require github.com/sashabaranov/go-openai v1.7.0 // indirect
--- playground/hello/main.go
+++ playground/hello/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	openai "github.com/sashabaranov/go-openai"
+)
 
 func main() {
-    fmt.Println("hello world")
+    // fmt.Println("hello world")
+    client := openai.NewClient("API_TOKEN_GOES_HERE")
+    resp, err := client.CreateChatCompletion(
+        context.Background(),
+        openai.ChatCompletionRequest{
+            Model: openai.GPT3Dot5Turbo,
+            Messages: []openai.ChatCompletionMessage{
+                {
+                    Role: openai.ChatMessageRoleUser,
+                    Content: "Hello",
+                },
+            },
+        },
+    )
+
+    if err != nil {
+        fmt.Printf("ChatCompletion error: %v\n", err)
+        return
+    }
+
+    fmt.Println(resp.Choices[0].Message.Content)
 }
