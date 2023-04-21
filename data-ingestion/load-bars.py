3985abd1e58d575600483a649f212c7ac8581819
Leo <leo.battalora@gmail.com>
2023-04-20 20:56:43 -0400

    ingest day bars

--- data-ingestion/load-bars.py
+++ data-ingestion/load-bars.py
@@ -1,19 +1,19 @@
 '''
 Description:
-This script pulls minute bars for an inputted ticker symbol for an inputted 
-date range and insert the data into the bars_minute_eastern PostgreSQL table in 
-RDS.
+This script pulls minute and day bars for inputted ticker symbols for an
+inputted date range and inserts the data into corresponding PostgreSQL tables
+in RDS.
 
 Usage:
-$ python load-bars-minute-eastern.py -t/--ticker INTC -s/--start 20220314 -e/--end 20220420
+$ python load-bars.py -t/--ticker NVDA,INTC -s/--start 20220314 -e/--end 20220420
 
 Required Environment Variables:
 ALPACA_API_KEY_ID, ALPACA_SECRET_KEY, PG_HOST, PG_PORT, PG_DB_NAME, 
 PG_USERNAME, PG_PASSWORD
 
-Details:
-Each weekday bars will be collected from 09:00 to 16:00 EST, except on 
-holidays where no bars will be collected.
+Notes:
+- Each weekday bars will be collected from 09:00 to 16:00 EST, except on 
+  holidays where no bars will be collected.
 '''
 
 import argparse
@@ -81,8 +81,9 @@ def main() -> int:
     # load holidays from properties
     holidays = loadHolidays('../properties/market-holidays.json')
 
-    # hard-code table name
-    PG_TABLE = 'bars_minute_eastern'
+    # hard-code table names
+    PG_TABLE_MINUTE = 'bars_minute_eastern'
+    PG_TABLE_DAY = 'bars_day'
 
     #--------------------------------------------------------------------------
     # Establish connections
@@ -120,19 +121,33 @@ def main() -> int:
         request_params = StockBarsRequest(symbol_or_symbols=tickers, timeframe=TimeFrame.Minute, start=start_dt, end=end_dt)
         stock_bars_minute = stock_client.get_stock_bars(request_params)
 
+        # pull day bars for tickers for day
+        print(f'[ INFO ] Fetching day bars for {tickers} on {current}')
+        request_params = StockBarsRequest(symbol_or_symbols=tickers, timeframe=TimeFrame.Day, start=(start_dt - datetime.timedelta(days=1)), end=start_dt)
+        stock_bars_day = stock_client.get_stock_bars(request_params)
+
         # convert alpaca data to DataFrame
         stock_bars_minute_df = stock_bars_minute.df
         stock_bars_minute_df.reset_index(inplace=True)
+        stock_bars_day_df = stock_bars_day.df
+        stock_bars_day_df.reset_index(inplace=True)
 
         # drop unused columns
         stock_bars_minute_df.drop('trade_count', axis=1, inplace=True)
         stock_bars_minute_df.drop('vwap', axis=1, inplace=True)
+        stock_bars_day_df.drop('trade_count', axis=1, inplace=True)
+        stock_bars_day_df.drop('vwap', axis=1, inplace=True)
 
         # timestamp to be timezone unaware (US/Eastern values)
         stock_bars_minute_df['timestamp'] = pd.to_datetime(stock_bars_minute_df['timestamp']).dt.tz_convert(pytz.timezone("US/Eastern")).dt.tz_localize(tz=None)
 
+        # day dataframe to only use date instead of datetime
+        stock_bars_day_df['date'] = pd.to_datetime(stock_bars_day_df['timestamp']).dt.date
+        stock_bars_day_df.drop('timestamp', axis=1, inplace=True)
+
         # insert rows and commit
-        stock_bars_minute_df.to_sql(name=PG_TABLE, con=conn, if_exists='append', index=False)
+        stock_bars_minute_df.to_sql(name=PG_TABLE_MINUTE, con=conn, if_exists='append', index=False)
+        stock_bars_day_df.to_sql(name=PG_TABLE_DAY, con=conn, if_exists='append', index=False)
         conn.commit()
 
         # go to next day
@@ -152,7 +167,7 @@ def main() -> int:
     # Exit program
     #--------------------------------------------------------------------------
 
-    print('[ INFO ] Exiting normally with code 0.') 
+    print('[ INFO ] Exiting normally with code 0.')
     return 0
 
 '''
