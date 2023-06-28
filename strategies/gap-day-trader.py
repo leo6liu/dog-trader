504b74d6dd76219c112de58f3260fbdf8d7c6409
ethanchang34 <ethanchang34@yahoo.com>
2023-06-28 20:24:52 +0000

    trade execution called immediately after gap detection

--- strategies/gap-day-trader.py
+++ strategies/gap-day-trader.py
@@ -23,6 +23,7 @@ def main():
     # TODO: If generalizable, add scanner implementation to generate ticker list
     # TODO: Understand which market conditions this work for/stick to one market
 
+
     # Tickers: AAPL, SBUX, TSLA, NVDA, GOOG, GOOGL, MSFT, AMZN, META, JNJ, JPM, XOM, PG, COST, AMD
     tickers = ['AAPL', 'SBUX']
 
@@ -68,49 +69,75 @@ def main():
     # print('Previous day bar:', previous_day_bar)
 
 
-    tickers_dict = {} # Dictionary with key as ticker symbol and value as the ticker object with other values
+    #------------------------------------------------------------------------------
+    # Create Ticker Objects and Store in Dictionary
+    #------------------------------------------------------------------------------
+    tickers_dict = {} # Dictionary with key as ticker symbol and value as the ticker object
     for ticker in tickers:
-        # Store high/low values into ticker objects
         previous_high = previous_day_bar[ticker][0].high
         previous_low = previous_day_bar[ticker][0].low
-        ticker_obj = Tickers(ticker, previous_high, previous_low)
-        tickers_dict[ticker_obj.symbol] = ticker_obj
+        ticker_obj = Tickers(ticker, previous_high, previous_low) # Store high/low values into ticker objects
+        tickers_dict[ticker_obj.symbol] = ticker_obj # Add ticker object to dictionary
         # print(f'\033[1m---{ticker}---\033[0m\nPrevious day high: {previous_high} \nPrevious day low: {previous_low}')
         
 
+    #------------------------------------------------------------------------------
+    # Stream Real-Time Stock Market Data
+    #------------------------------------------------------------------------------
+    gap_orders = [] # The limit orders that get opened at 9:30 if there's a gap and used later to close any unfilled gap orders
     # Async handler
     async def quote_data_handler(data):
         global all_gaps_checked
-
+        
+        # If all gaps are checked, there's no need to check each ticker again
         if not all_gaps_checked:
+
+            # If the ticker has already been checked, we don't need to check it again
             if not tickers_dict[data.symbol].gap_check:
                 market_price = data.ask_price # Assume market price is the ask price since that's what we'd buy at
-                if market_price > 1.03 * tickers_dict[data.symbol].high:
-                    tickers_dict[data.symbol].gap_up = True
-                elif market_price < 0.98 * tickers_dict[data.symbol].low:
-                    tickers_dict[data.symbol].gap_down = True
+
+                if market_price > 1.03 * tickers_dict[data.symbol].high: # Only consider a 3% gap up
+
+                    tickers_dict[data.symbol].gap_up = True # [COULD BE REDUNDANT]
+
+                    # Request sell limit order
+                    order_data = LimitOrderRequest(symbol=data.symbol, limit_price=previous_high-0.02, qty=100, side=OrderSide.SELL, time_in_force=TimeInForce.DAY)
+                    limit_order = trading_client.submit_order(order_data=order_data)
+                    gap_orders.append(limit_order.id)
+
+                elif market_price < 0.98 * tickers_dict[data.symbol].low: # Only consider a 2% gap down
+
+                    tickers_dict[data.symbol].gap_down = True # [COULD BE REDUNDANT]
+
+                    # Request buy limit order
+                    order_data = LimitOrderRequest(symbol=data.symbol, limit_price=previous_low+0.02, qty=100, side=OrderSide.BUY, time_in_force=TimeInForce.DAY)
+                    limit_order = trading_client.submit_order(order_data=order_data)
+                    gap_orders.append(limit_order.id) # Store order_id to cancel the order if it's not filled
+                    
                 tickers_dict[data.symbol].gap_check = True
             
             # Check if all_gaps_checked should now be True
             all_gaps_checked = True
             for t_obj in tickers_dict.values():
+
                 # If a ticker's gap is not checked, then all_gaps_checked is False
                 if not t_obj.gap_check:
                     all_gaps_checked = False
 
-        # Quote data will arrive here
-        print(data)
+        # If all tickers are checked, we can close the real-time data stream       
+        else:
+            stock_stream.close()
+
+
+        print(data) # Quote data
     stock_stream.subscribe_quotes(quote_data_handler, 'AAPL', 'SBUX')
     stock_stream.run()
 
 
-    # Gap orders (the limit orders that get opened at 9:30 if there's a gap)
-    gap_orders = []
+    print('OUTSIDE WEBSOCKET~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
     for t_obj in tickers_dict.values():
         #------------------------------------------------------------------------------
         # TRADE CONDITIONS AND EXECUTIONS
-        # TODO: Pull today's opening data
-        # TODO: Check gap condition
         # TODO: Set custom time expiration on limit orders
         # TODO: More difficult than using expiration, but hardcode a timer to cancel orders by ID if their status is not filled
         # TODO: Set stop loss
@@ -118,21 +145,8 @@ def main():
         # TODO: Consider fill speed
         # TODO: Set FPO condition
         #------------------------------------------------------------------------------
-        gap_up = False
-        gap_down = False
-        # [Conditions for gap up and gap down go HERE]
-        if gap_down:
+        print('foobar')
 
-            order_data = LimitOrderRequest(symbol=t_obj.symbol, limit_price=previous_low+0.02, qty=100, side=OrderSide.BUY, time_in_force=TimeInForce.DAY)
-            limit_order = trading_client.submit_order(order_data=order_data)
-            gap_orders.append(limit_order.id) # Store order_id to cancel the order if it's not filled
-        elif gap_up:
-
-            order_data = LimitOrderRequest(symbol=t_obj.symbol, limit_price=previous_high-0.02, qty=100, side=OrderSide.SELL, time_in_force=TimeInForce.DAY)
-            limit_order = trading_client.submit_order(order_data=order_data)
-            gap_orders.append(limit_order.id)
-        gap_up = False
-        gap_down = False  
 
 
 class Tickers:
