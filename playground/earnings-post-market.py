a3c3debeebdb148518473b3f47966ceae9ce5f37
Ethan <ethanchang34@yahoo.com>
2023-08-09 01:00:17 -0400

    function for creating time array to clean up main func

--- playground/earnings-post-market.py
+++ playground/earnings-post-market.py
@@ -8,6 +8,7 @@ __email__ = "ethanchang34@yahoo.com"
 
 import datetime
 from datetime import timezone
+from typing import List
 import pytz
 import pandas as pd
 import time
@@ -19,114 +20,99 @@ import yfinance as yf
 from alpaca.data import StockHistoricalDataClient, TimeFrame, StockBarsRequest
 
 
-# declare ticker
-ticker = "TSLA"
+def main():
+    # TODO: Just pull date as for first column to clean up datetime look
+    # TODO: Error handling on cases where there's no price due to lack of volume
 
-# get Alpaca environment variables
-API_KEY = os.getenv('APCA_API_KEY_ID')
-SECRET_KEY = os.getenv('APCA_API_SECRET_KEY')
+    # declare ticker
+    ticker = "TSLA"
 
-# check for missing alpaca environment variables
-if API_KEY is None or SECRET_KEY is None:
-    print('[ ERROR ] Environment variables ALPACA_API_KEY_ID or ALPACA_SECRET_KEY not found.')
-    print('[ INFO ] Exiting...')
-    quit()
+    # get Alpaca environment variables
+    API_KEY = os.getenv('APCA_API_KEY_ID')
+    SECRET_KEY = os.getenv('APCA_API_SECRET_KEY')
 
-# create Alpaca clients
-stock_client = StockHistoricalDataClient(API_KEY, SECRET_KEY)
+    # check for missing alpaca environment variables
+    if API_KEY is None or SECRET_KEY is None:
+        print('[ ERROR ] Environment variables ALPACA_API_KEY_ID or ALPACA_SECRET_KEY not found.')
+        print('[ INFO ] Exiting...')
+        quit()
 
-# create empty dataframe to populate and export as csv at the end
-data = {
-    'Date': [],
-    '4:00PM': [],
-    '4:01PM': [],
-    '4:05PM': [],
-    '4:30PM': [],
-    '5:00PM': [],
-    '5:30PM': [],
-    '6:00PM': [],
-    '6:30PM': [],
-    '7:00PM': []
-}
-earnings_df = pd.DataFrame(data)
+    # create Alpaca clients
+    stock_client = StockHistoricalDataClient(API_KEY, SECRET_KEY)
 
-# get earnings dates
-today = datetime.datetime.now(tz=timezone.utc)
-df = yf.Ticker(ticker)
-earnings_dates = df.get_earnings_dates(limit=28).index
-
-for date in earnings_dates:
-    # convert Timestamp to datetime and set to 4PM
-    datetime_obj = date.to_pydatetime().replace(hour=16, minute = 0, second = 0)
-    
-    new_row = []
-    # only grab dates in the past
-    if datetime_obj < today:
-        new_row.append(datetime_obj)
-
-        request_params = StockBarsRequest(symbol_or_symbols=ticker, start=datetime_obj, end=datetime_obj, timeframe=TimeFrame.Minute)
-        price = stock_client.get_stock_bars(request_params)[ticker][0]
-        new_row.append(price.close)
-        # print(datetime_obj, price.close, price.timestamp)
-
-        newtime = datetime_obj + datetime.timedelta(minutes=1)
-        request_params = StockBarsRequest(symbol_or_symbols=ticker, start=newtime, end=newtime, timeframe=TimeFrame.Minute)
-        price = stock_client.get_stock_bars(request_params)[ticker][0]
-        new_row.append(price.close)
-
-        newtime = datetime_obj + datetime.timedelta(minutes=5)
-        request_params = StockBarsRequest(symbol_or_symbols=ticker, start=newtime, end=newtime, timeframe=TimeFrame.Minute)
-        price = stock_client.get_stock_bars(request_params)[ticker][0]
-        new_row.append(price.close)
-
-        newtime = datetime_obj + datetime.timedelta(minutes=30)
-        request_params = StockBarsRequest(symbol_or_symbols=ticker, start=newtime, end=newtime, timeframe=TimeFrame.Minute)
-        price = stock_client.get_stock_bars(request_params)[ticker][0]
-        new_row.append(price.close)
-
-        newtime = datetime_obj + datetime.timedelta(hours=1)
-        request_params = StockBarsRequest(symbol_or_symbols=ticker, start=newtime, end=newtime, timeframe=TimeFrame.Minute)
-        price = stock_client.get_stock_bars(request_params)[ticker][0]
-        new_row.append(price.close)
-
-        newtime = datetime_obj + datetime.timedelta(hours=1, minutes=30)
-        request_params = StockBarsRequest(symbol_or_symbols=ticker, start=newtime, end=newtime, timeframe=TimeFrame.Minute)
-        price = stock_client.get_stock_bars(request_params)[ticker][0]
-        new_row.append(price.close)
-
-        newtime = datetime_obj + datetime.timedelta(hours=2)
-        request_params = StockBarsRequest(symbol_or_symbols=ticker, start=newtime, end=newtime, timeframe=TimeFrame.Minute)
-        price = stock_client.get_stock_bars(request_params)[ticker][0]
-        new_row.append(price.close)
-
-        newtime = datetime_obj + datetime.timedelta(hours=2, minutes=30)
-        request_params = StockBarsRequest(symbol_or_symbols=ticker, start=newtime, end=newtime, timeframe=TimeFrame.Minute)
-        price = stock_client.get_stock_bars(request_params)[ticker][0]
-        new_row.append(price.close)
-
-        newtime = datetime_obj + datetime.timedelta(hours=3)
-        request_params = StockBarsRequest(symbol_or_symbols=ticker, start=newtime, end=newtime, timeframe=TimeFrame.Minute)
-        price = stock_client.get_stock_bars(request_params)[ticker][0]
-        new_row.append(price.close)
-
-        print(new_row)
+    # create empty dataframe to populate and export as csv at the end
+    data = {
+        'Date': [],
+        '4:00PM': [],
+        '4:01PM': [],
+        '4:05PM': [],
+        '4:30PM': [],
+        '5:00PM': [],
+        '5:30PM': [],
+        '6:00PM': [],
+        '6:30PM': [],
+        '7:00PM': []
+    }
+    earnings_df = pd.DataFrame(data)
 
 
-# datetime_obj = datetime.datetime(2023, 8, 7, 19, 45, 0, 0, tzinfo=timezone.utc)
-# request_params = StockBarsRequest(symbol_or_symbols=ticker, start=datetime_obj, end=datetime_obj, timeframe=TimeFrame.Minute)
-# response = stock_client.get_stock_bars(request_params)[ticker][0].close
-# print(response)
+    # get earnings dates
+    today = datetime.datetime.now(tz=timezone.utc)
+    df = yf.Ticker(ticker)
+    earnings_dates = df.get_earnings_dates(limit=28).index
+
+    for date in earnings_dates:
+        # convert Timestamp to datetime and set to 4PM
+        datetime_obj = date.to_pydatetime().replace(hour=16, minute = 0, second = 0)
+        time_arr = create_time_arr(datetime_obj)
+        
+        new_row = []
+        # only grab dates in the past
+        if datetime_obj < today:
+            new_row.append(datetime_obj) # [JUST PULL DATE]
+
+            for time in time_arr:
+                request_params = StockBarsRequest(symbol_or_symbols=ticker, start=time, end=time, timeframe=TimeFrame.Minute)
+                price = stock_client.get_stock_bars(request_params)[ticker][0]
+                new_row.append(price.close)
+
+            print(new_row)
 
 
-# start_dt = datetime(2023, 1, 27, 14, 40, 0, 0, tzinfo=timezone.utc) # 2023/01/27 14:40 UTC
-# end_dt = datetime(2023, 1, 27, 14, 42, 0, 0, tzinfo=timezone.utc) # 2023/01/27 14:42 UTC
+    # datetime_obj = datetime.datetime(2023, 8, 7, 19, 45, 0, 0, tzinfo=timezone.utc)
+    # request_params = StockBarsRequest(symbol_or_symbols=ticker, start=datetime_obj, end=datetime_obj, timeframe=TimeFrame.Minute)
+    # response = stock_client.get_stock_bars(request_params)[ticker][0].close
+    # print(response)
 
-# ------------------------------------------------------------------------------
-# Example of getting alpaca data (need changes to minutes and `looping list date)
-# ------------------------------------------------------------------------------
-# start_dt = get_last_weekday()
-#     end_dt = start_dt + datetime.timedelta(days=1)
-#     print("[ INFO ] Fetching previous day bar for", ", ".join(tickers))
-#     request_params = StockBarsRequest(symbol_or_symbols=tickers, start=start_dt, end=end_dt, timeframe=TimeFrame.Day)
-#     previous_day_bar = stock_client.get_stock_bars(request_params)
-#     # print('Previous day bar:', previous_day_bar)
+
+    # start_dt = datetime(2023, 1, 27, 14, 40, 0, 0, tzinfo=timezone.utc) # 2023/01/27 14:40 UTC
+    # end_dt = datetime(2023, 1, 27, 14, 42, 0, 0, tzinfo=timezone.utc) # 2023/01/27 14:42 UTC
+
+    # ------------------------------------------------------------------------------
+    # Example of getting alpaca data (need changes to minutes and `looping list date)
+    # ------------------------------------------------------------------------------
+    # start_dt = get_last_weekday()
+    #     end_dt = start_dt + datetime.timedelta(days=1)
+    #     print("[ INFO ] Fetching previous day bar for", ", ".join(tickers))
+    #     request_params = StockBarsRequest(symbol_or_symbols=tickers, start=start_dt, end=end_dt, timeframe=TimeFrame.Day)
+    #     previous_day_bar = stock_client.get_stock_bars(request_params)
+    #     # print('Previous day bar:', previous_day_bar)
+
+
+def create_time_arr(datetime_obj) -> List[datetime.datetime]:
+    """Create an array of datetimes: 4:01PM, 4:05PM, etc."""
+    time_arr = []
+    time_arr.append(datetime_obj)
+    time_arr.append(datetime_obj + datetime.timedelta(minutes=1))
+    time_arr.append(datetime_obj + datetime.timedelta(minutes=5))
+    time_arr.append(datetime_obj + datetime.timedelta(minutes=30))
+    time_arr.append(datetime_obj + datetime.timedelta(hours=1))
+    time_arr.append(datetime_obj + datetime.timedelta(hours=1, minutes=30))
+    time_arr.append(datetime_obj + datetime.timedelta(hours=2))
+    time_arr.append(datetime_obj + datetime.timedelta(hours=2, minutes=30))
+    time_arr.append(datetime_obj + datetime.timedelta(hours=3))
+    return time_arr
+
+
+if __name__ == '__main__':
+    sys.exit(main())
