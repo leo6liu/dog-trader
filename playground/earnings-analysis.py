d4a120bb3da153cf77724ff89fa6893e1c3f66de
Ethan <ethanchang34@yahoo.com>
2023-08-12 17:09:45 -0400

    AAPL specific analysis

--- playground/earnings-analysis.py
+++ playground/earnings-analysis.py
@@ -18,18 +18,36 @@ for file_name in file_names:
     tickers.append(file_name.split("_")[0])
 
 # open csv files
-for ticker in tickers: # loop through tickers rather than csv files so we can use 'ticker' as a variable
-    with open(f"./earnings/{ticker}_earnings.csv") as earnings_csv:
-        reader = csv.reader(earnings_csv, delimiter=",")
+# for ticker in tickers: # loop through tickers rather than csv files so we can use 'ticker' as a variable
+#     with open(f"./earnings/{ticker}_earnings.csv") as earnings_csv:
+#         reader = csv.reader(earnings_csv, delimiter=",")
 
-        # analysis
-        same_direction = 0
-        length = 0
-        for row in reader:
-            # fraction of scenarios where movement direction from 4:00 to 4:05 is the same as direction from 4:05 to 4:30
-            if row[4] > row[2] and row[5] > row[4]:
-                same_direction += 1
-            elif row[4] < row[2] and row[5] < row[4]:
+#         # analysis
+#         same_direction = 0
+#         length = 0
+#         next(reader) # skip the first row
+#         for row in reader:
+#             # fraction of scenarios where movement direction from 4:00 to 4:05 is the same as direction from 4:05 to 4:30
+#             # numbers represented as strings can be compared, no need to convert to float
+#             if row[4] > row[2] and row[5] > row[4]:
+#                 same_direction += 1
+#             elif row[4] < row[2] and row[5] < row[4]:
+#                 same_direction += 1
+#             length += 1
+#         print(f"{ticker} 4:00 -> 4:05 and 4:05 -> 4:00 same direction: {same_direction/length}")
+
+ticker = "AAPL"
+with open(f"./earnings/{ticker}_earnings.csv") as earnings_csv:
+    reader = csv.reader(earnings_csv, delimiter=",")
+    same_direction = 0
+    length = 0
+    next(reader)
+
+    for row in reader:
+        if row[4] > row[3]:
+            if row[8] > row[4]:
                 same_direction += 1
             length += 1
-        print(f"{ticker} 4:00 -> 4:05 and 4:05 -> 4:00 same direction: {same_direction/length}")
+
+    print(same_direction, length)
+    print(f"{ticker} 4:01 -> 4:05 -> 6:00 same direction: {same_direction/length}")
